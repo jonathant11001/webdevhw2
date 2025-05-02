0bc6db757ac29d9762d4af2efb528f29f9e74fe5
unknown <jonathant11001@gmail.com>
2025-05-02 10:42:43 -0400

    fixed routes

--- resturant/src/App.jsx
+++ resturant/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -10,16 +10,18 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </div>
+    <Router basename="/webdevhw2">
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
