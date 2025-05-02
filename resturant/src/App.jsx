0a77f06326621121e96d4ec68b8766133ffc42c9
unknown <jonathant11001@gmail.com>
2025-05-02 10:52:19 -0400

    fixed deployment issue

--- resturant/src/App.jsx
+++ resturant/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './index.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -10,18 +10,16 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <Router basename="/webdevhw2">
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
